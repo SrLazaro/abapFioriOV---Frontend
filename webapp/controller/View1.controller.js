dbbe836f05ca57347a3c33f7f4c7967108e1f832
SrLazaro <lazarosobreira@gmail.com>
2026-05-07 23:03:41 -0300

    Implementação da i18n

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/base/strings/formatMessage"
+], (Controller, MessageToast, formatMessage) => {
     "use strict";
 
     return Controller.extend("zov001.controller.View1", {
+
         onInit() {
 
+            var oView = this.getView();
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.setData({"usuario": {"nome": "ABAPer"}});
+            oView.setModel(oModel);
+
+        },
+
+        formatWelcomeMessage: function (sMessage, sNome) {
+            return formatMessage(sMessage, [sNome]);
         },
 
         onHelloWorldButtonPress: function () {
@@ -17,6 +28,22 @@ sap.ui.define([
             this.getView()
                 .getModel()
                 .refresh(true);
+
+            /*var sMessageRefresh = this.getView()
+                                      .getModel("i18n")
+                                      .getResourceBundle()
+                                      .getText("messageRefresh");
+            
+            MessageToast.show(sMessageRefresh);*/
+
+            var oI18n = this.getView().getModel("i18n").getResourceBundle();
+            var oData = this.getView().getModel().getData();
+
+            var sMessageRefresh = oI18n.getText("messageRefresh",
+                                                [oData.usuario.nome]
+                                                );
+            
+            MessageToast.show(sMessageRefresh);
 
         }
     });
