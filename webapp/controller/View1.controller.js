ed01ef35c27d25bf77eefe066d860a224436faa0
SrLazaro <lazarosobreira@gmail.com>
2026-05-11 13:44:05 -0300

    Implementando Model

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,44 +7,169 @@ sap.ui.define([
 
     return Controller.extend("zov001.controller.View1", {
 
-        onInit() {
+        /*onInit() {
 
             var oView = this.getView();
+
+            // Model padrão da View
             var oModel = new sap.ui.model.json.JSONModel();
-            oModel.setData({"usuario": {"nome": "ABAPer"}});
+
+            var sNome = sap.ushell.Container
+                .getService("UserInfo")
+                .getUser()
+                .getFullName();
+
+            oModel.setData({
+                usuario: {
+                    nome: sNome
+                }
+            });
+
             oView.setModel(oModel);
+
+            // Model nomeado "dados"
+            var oModelDados = new sap.ui.model.json.JSONModel();
+
+            oModelDados.setData({
+                usuario: {
+                    nome: "bob marley"
+                }
+            });
+
+            oView.setModel(oModelDados, "dados");
+
+        }*/
+       
+       onInit: function () {
+
+        var oView = this.getView();
+
+        // Model de usuário
+        var oUserModel = new sap.ui.model.json.JSONModel();
+
+        var sNome = sap.ushell.Container
+            .getService("UserInfo")
+            .getUser()
+            .getFullName();
+
+        oUserModel.setData({
+            usuario: {
+                nome: sNome
+            }
+        });
+
+        oView.setModel(oUserModel, "user");
+
+        // Model "dados"
+        var oDadosModel = new sap.ui.model.json.JSONModel();
+
+        oDadosModel.setData({
+            usuario: {
+                nome: "bob marley"
+            }
+        });
+
+        oView.setModel(oDadosModel, "dados");
+
+    },
+
+        onTestarDaMassa: function () {
+
+            // =========================
+            // MODEL I18N
+            // =========================
+
+            var oI18n = this.getView()
+                .getModel("i18n")
+                .getResourceBundle();
+
+            var sText = oI18n.getText("testadaMarota");
+            var sSeparador = oI18n.getText("separador");
+
+            console.log("Texto i18n:");
+            console.log(sText);
+
+            console.log(sSeparador);
+
+            // =========================
+            // MODEL USUARIOS
+            // =========================
+
+            var oUsuariosModel = this.getOwnerComponent()
+                .getModel("usuarios");
+
+            var oUsuariosData = oUsuariosModel.getData();
+
+            console.log("Model usuários:");
+            console.log(oUsuariosData);
+
+            console.log(sSeparador);
+
+            // =========================
+            // MODEL ODATA
+            // =========================
+
+            var oServiceModel = this.getOwnerComponent()
+                .getModel();
+
+            oServiceModel.read("/OVCabSet", {
+
+                success: function (oData, oResponse) {
+
+                    console.log("Dados retornados do serviço:");
+
+                    console.log(oData);
+                    console.log(oResponse);
+
+                },
+
+                error: function (oErro) {
+
+                    console.log("Deu erro:");
+
+                    console.log(oErro);
+
+                }
+
+            });
 
         },
 
         formatWelcomeMessage: function (sMessage, sNome) {
+
             return formatMessage(sMessage, [sNome]);
+
         },
 
         onHelloWorldButtonPress: function () {
+
             MessageToast.show("Hello World");
+
         },
-        
+
         onModelRefresh: function () {
+
             this.getView()
                 .getModel()
                 .refresh(true);
 
-            /*var sMessageRefresh = this.getView()
-                                      .getModel("i18n")
-                                      .getResourceBundle()
-                                      .getText("messageRefresh");
-            
-            MessageToast.show(sMessageRefresh);*/
+            var oI18n = this.getView()
+                .getModel("i18n")
+                .getResourceBundle();
 
-            var oI18n = this.getView().getModel("i18n").getResourceBundle();
-            var oData = this.getView().getModel().getData();
+            var oData = this.getView()
+                .getModel()
+                .getData();
 
-            var sMessageRefresh = oI18n.getText("messageRefresh",
-                                                [oData.usuario.nome]
-                                                );
-            
+            var sMessageRefresh = oI18n.getText(
+                "messageRefresh",
+                [oData.usuario.nome]
+            );
+
             MessageToast.show(sMessageRefresh);
 
         }
+
     });
+
 });
