2eeddf06e20e3565f5b8111dd365ba5b2be553f9
SrLazaro <lazarosobreira@gmail.com>
2026-05-14 22:40:56 -0300

    Implementando Data Biding

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -71,7 +71,89 @@ sap.ui.define([
 
         oView.setModel(oDadosModel, "dados");
 
+        // Data Binding - OneWay
+
+        var oModel1 = new sap.ui.model.json.JSONModel();
+        oModel1.setData(
+            {
+                "funcionario": {
+                    "nome": "Zero" 
+                }
+            }
+        );
+        oModel1.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+
+        // Data Binding - TwoWay
+
+        var oModel2 = new sap.ui.model.json.JSONModel();
+        oModel2.setData(
+            {
+                "funcionario": {
+                    "nome": "Megaman"
+                }
+            }
+        );
+        oModel2.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+
+        // Data Binding - One Time
+
+        var oModel3 = new sap.ui.model.json.JSONModel();
+        oModel3.setData(
+            {
+                "funcionario": {
+                    "nome": "Axl"
+                }
+            }
+        );
+        oModel3.setDefaultBindingMode(sap.ui.model.BindingMode.OneTime);
+
+
+        oView.setModel(oModel1, "oneway");
+        oView.setModel(oModel2, "twoway");
+        oView.setModel(oModel3, "onetime");
+
+
     },
+
+        onTestOneWay: function (){
+
+            var oView = this.getView();
+            var oModel = oView.getModel("oneway");
+            var oData = oModel.getData();
+
+            oData.funcionario.nome += "!";
+
+            oModel.setData(oData);
+            //oView.setModel(oModel, "oneway");
+        },
+
+        onTestTwoWay: function (){
+
+            var oView = this.getView();
+            var oModel = oView.getModel("twoway");
+            var oData = oModel.getData();
+
+            oData.funcionario.nome += "!";
+
+            oModel.setData(oData);
+            //oView.setModel(oModel, "twoway");
+
+        },
+
+        onTestOneTime: function (){
+
+            var oView = this.getView();
+            
+            var oModel = oView.getModel("onetime");
+            var oData = oModel.getData();
+
+            debugger;
+
+            oData.funcionario.nome += "?";
+            oModel.setData(oData);
+            //oView.setModel(oModel, "onetime");
+
+        },
 
         onTestarDaMassa: function () {
 
