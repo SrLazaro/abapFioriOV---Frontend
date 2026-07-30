cedf42757734db79ef13dc979da739a8a1f0b5c4
SrLazaro <lazarosobreira@gmail.com>
2026-07-30 11:37:37 -0300

    Implementando OV criação - CRUDQ CREATE

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -10,28 +10,8 @@ sap.ui.define([
                 decimalSeparator: ',',
                 decimals: 2
             };
-            
             var oFloatFormat = sap.ui.core.format.NumberFormat.getFloatInstance(oFormatOptions);
             return oFloatFormat.format(parseFloat(sValue));
-        },
-
-        formatCPF: function(sValue){
-            // removendo tudo, exceto números
-            sValue = sValue.replace(/\D/g, '');
-
-            // adicionando pontuação
-            sValue = sValue.slice(0,3)+"."+
-                     sValue.slice(3,6)+"."+
-                     sValue.slice(6,9)+"-"+
-                     sValue.slice(9,11);
-
-
-            return sValue;
-        },
-
-        formatStatus: function(sValue){
-            const oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-			return oResourceBundle.getText("Status"+sValue);
         }
     };
 });
